082d8f2f2a2cf65a82137c532c7466e38bc7bf12
hafeezrkhan <khanh2013@fau.edu>
2015-07-10 18:30:38 -0400

    Assignment 4
    Assignment 4

--- Assignment4.py
+++ Assignment4.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 #C:\Python27\Scripts\SUMMARYMEASURESOFHEALTH.csv
 #C:\Python27\Scripts\DEMOGRAPHICS.csv
 #C:\Python27\Scripts\ANALYTICS.csv
@@ -23,3 +26,16 @@ with open('C:\Python27\Scripts\ANALYTICS.csv', 'wb') as f:
     w = csv.writer(f)
     for key, value in result.iteritems():
         w.writerow([key] + value)
+
+s=pd.read_csv("C:\Python27\Scripts\MYANALYTICS.csv",parse_dates=['CHSI_State_Name'])
+s.head()
+s.describe()
+s['CHSI_State_Name'].describe()
+s.dtypes
+c = s[['CHSI_State_Name','Population_Size','Poverty']]
+c.head()
+c_group = c.groupby('CHSI_State_Name')
+c_group.size()
+s_t = c_group.sum()
+s_t.sort(columns='Poverty').head()
+my_plot = s_t.plot(kind='bar')
